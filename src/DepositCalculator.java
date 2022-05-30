69c5a2a16e6ccc32e0e75c5600f8c0de35f4488d
Roman <madara-09@bk.ru>
2022-05-30 14:01:28 +0300

    Отредактирован код

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double y, int d ) {
-       double pay = a * Math.pow((1 + y / 12), 12 * d);
-          return roundUp(pay, 2);
+    double calculateComplexPercent(double a, double yearRate, int depositPeriod) {
+       double pay = a * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+       return roundUp(pay, 2);
   }
 
-    double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) {
-         return roundUp(doubleAmount+doubleAmount * double_year_rate *deposit_period, 2);
+    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
+        return roundUp(amount + amount * yearRate * depositPeriod, 2);
     }
 
     double roundUp(double value, int places) {
-       double ScaLe= Math.pow(10, places);
-       return Math.round(value * ScaLe) / ScaLe;
+        double ScaLe= Math.pow(10, places);
+        return Math.round(value * ScaLe) / ScaLe;
     }
 
     void printMenu() {
-        int period, action ;
+        int period, action;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите сумму вклада в рублях:");
-        int amount = scanner.nextInt();
+        int amountRub = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt( );
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
@@ -28,14 +28,14 @@ public class DepositCalculator {
         double out = 0;
 
         if (action == 1) {
-            out = calculateSimplePercent(amount, 0.06, period);
+            out = calculateSimplePercent(amountRub, 0.06, period);
         } else if (action == 2) {
-            out = calculateComplexPercent(amount, 0.06, period);
+            out = calculateComplexPercent(amountRub, 0.06, period);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
+        System.out.println("Результат вклада: " + amountRub + " за " + period + " лет превратятся в " + out);
         }
 
     public static void main(String[] args) {
         new DepositCalculator().printMenu();
-}
+    }
 }
