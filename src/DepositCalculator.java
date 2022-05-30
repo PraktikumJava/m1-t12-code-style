52a50780bfb760b91664d91ba4c7a9edb2dab456
Roman <madara-09@bk.ru>
2022-05-30 17:02:18 +0300

    Отредактирован код

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double yearRate, int depositPeriod) {
-       double pay = a * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
-       return roundUp(pay, 2);
-  }
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return roundUp(pay, 2);
+    }
 
     double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
         return roundUp(amount + amount * yearRate * depositPeriod, 2);
     }
 
     double roundUp(double value, int places) {
-        double ScaLe= Math.pow(10, places);
+        double ScaLe = Math.pow(10, places);
         return Math.round(value * ScaLe) / ScaLe;
     }
 
@@ -22,7 +22,7 @@ public class DepositCalculator {
         System.out.println("Введите сумму вклада в рублях:");
         int amountRub = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
-        period = scanner.nextInt( );
+        period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
         double out = 0;
@@ -33,7 +33,7 @@ public class DepositCalculator {
             out = calculateComplexPercent(amountRub, 0.06, period);
         }
         System.out.println("Результат вклада: " + amountRub + " за " + period + " лет превратятся в " + out);
-        }
+    }
 
     public static void main(String[] args) {
         new DepositCalculator().printMenu();
