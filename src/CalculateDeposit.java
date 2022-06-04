ae489673a0ef094b69ab1c8161d948982d763cd3
Edmecom <104772320+Edmecom@users.noreply.github.com>
2022-06-04 22:48:33 +0300

    Add files via upload

---
+++ src/CalculateDeposit.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CalculateDeposit {
+    double calculateComplexPercent(double amount, double yearRate, int period ) {
+        double value = amount * Math.pow((1 + yearRate / 12), 12 * period);
+        return calculateAmount(value, 2);
+    }
+
+    double calculateSimplePercent(double amount, double yearRate, int period) {
+         return calculateAmount(amount + amount * yearRate * period, 2);
+    }
+
+    double calculateAmount(double value, int places) {
+        double ScaLe = Math.pow(10, places);
+        return Math.round(value * ScaLe) / ScaLe;
+    }
+
+    void printMenu() {
+        int period, action;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:") ;
+        period = scanner.nextInt( );
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        double outDoubleVar = 0;
+        if (action ==1) {
+            outDoubleVar = calculateSimplePercent(amount, 0.06, period);
+        } else if (action == 2) {
+            outDoubleVar = calculateComplexPercent(amount, 0.06, period);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+    }
+
+    public static void main(String[] args) {
+        new CalculateDeposit().printMenu();
+    }
+}
