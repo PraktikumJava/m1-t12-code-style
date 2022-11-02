ce4cb0141af34b6e03732a8bd7806e08e33db0aa
Oleg Bobrikov <bobrikov@yandex.ru>
2022-11-02 21:46:03 +0300

    code style4

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double y, int d) {
-        double pay = a * Math.pow((1 + y / 12), 12 * d);
+    double calculateComplexPercent(double amount, double annualInterestRate, int years) {
+        double pay = amount * Math.pow((1 + annualInterestRate / 12), 12 * years);
         return round(pay, 2);
     }
 
-    double calculateSimplePercent(double amount, double annualInterestRate, int months) {
-        return round(amount + amount * annualInterestRate * months, 2);
+    double calculateSimplePercent(double amount, double annualInterestRate, int years) {
+        return round(amount + amount * annualInterestRate * years, 2);
     }
 
     double round(double value, int places) {
@@ -16,23 +16,24 @@ public class DepositCalculator {
     }
 
     void calculateIncomeOnADeposit() {
-        int period, action;
+        int years;
+        int action;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
 
         int amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
-        period = scanner.nextInt();
+        years = scanner.nextInt();
 
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
         double investmentResult = 0.0;
         if (action == 1) {
-            investmentResult = calculateSimplePercent(amount, 0.06, period);
+            investmentResult = calculateSimplePercent(amount, 0.06, years);
         } else if (action == 2) {
-            investmentResult = calculateComplexPercent(amount, 0.06, period);
+            investmentResult = calculateComplexPercent(amount, 0.06, years);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + investmentResult);
+        System.out.println("Результат вклада: " + amount + " за " + years + " лет превратятся в " + investmentResult);
     }
 
     public static void main(String[] args) {
