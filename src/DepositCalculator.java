66fc66e98702b58d6eae9ecab13a315cbc02eeee
mrmaximum80 <93443831+mrmaximum80@users.noreply.github.com>
2023-01-11 13:29:35 +0300

    comments

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double firstVal, double secondVal, int thirdVal) {
+    double calculateComplexPercent(double firstVal, double secondVal, int thirdVal) {   // тут бы я поменял имена параметров как в calculateSimplePercent
         double pay = firstVal * Math.pow((1 + secondVal / 12), 12 * thirdVal);
         return round(pay, 2);
     }
@@ -15,7 +15,7 @@ public class DepositCalculator {
         return Math.round(value * scale) / scale;
     }
 
-    void doImportantJob() {
+    void doImportantJob() { // тут бы тоже сменил название метода на более подходящее
         int period, action;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
@@ -31,7 +31,7 @@ public class DepositCalculator {
         else if (action == 2) {
             outVar = calculateComplexPercent(amount, 0.06, period);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar); // вопрос: тут outDoubleVar, выше outVar. Не должно работать
     }
 
     public static void main(String[] args) {
